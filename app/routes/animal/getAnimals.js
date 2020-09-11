4808b40f6a8624a043dd29c8bfca59f1537d7e7c
941485-patrol <dribbleroar@gmail.com>
2020-09-11 18:59:49 +0800

    add example query

--- app/routes/animal/getAnimals.js
+++ app/routes/animal/getAnimals.js
@@ -7,7 +7,16 @@ const knex = require('../../../knex/knex.js');
 const getAnimals = async (req, res, next)=>
   {
     try {
-      var animals = await knex('animals').select();
+      var x = 'nIm'
+      var perPage = 5;
+      var page = req.query.page == undefined ? 1 : req.query.page;
+      var pageSkip = parseInt(page) - parseInt(1);
+      var animals = await knex('animals')
+        .select('animals.*', 'status.name AS statName')
+        .leftJoin('status', 'animals.status_id', 'status.id')
+        .where('animals.name', 'ilike', `%${x}%`)
+        .limit(perPage).offset(pageSkip*perPage)
+        .orderBy('animals.id');
       res.status(200).json(animals);
     } catch (error) {
       console.log(error);
